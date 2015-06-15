6615962932eca5642b9f2c7cf9098d25cc3c3941
stephenfortune <stephen.j.fortune@gmail.com>
2015-06-15 18:10:33 +0100

    adding pseudocode to the transaction controller

--- transaction_controller.rb
+++ transaction_controller.rb
@@ -5,7 +5,13 @@ Mongoid::Config.load!('./mongoid.yml', :development)
 
 class TransactionController
 
-  def transfer(payee, amount, date)
+  def transfer(payer, payee, amount, date, credit)
+    # e.g. String "stephen", amount as float, and date in someformat
+
+    # # psuedocode for now
+    # payeeId = Customer.where(name: payee).find(_id)
+    # payerId = Customer.where(name: payee).find(_id)
+    # Transaction.create(amount: amount, date: date, CrDr: credit, [customer_ids])
 
   end
 
