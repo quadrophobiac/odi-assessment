50ecc29e547c7b637224e09add6ad8346a10b593
stephenfortune <stephen.j.fortune@gmail.com>
2015-06-15 18:35:32 +0100

    http://www.rubydoc.info/github/mongoid/mongoid/Mongoid/Extensions/Date mongoID and ruby have a v. particular way of formatting dates

--- transaction_controller.rb
+++ transaction_controller.rb
@@ -6,7 +6,7 @@ Mongoid::Config.load!('./mongoid.yml', :development)
 class TransactionController
 
   def transfer(payer, payee, amount, date, credit)
-    # e.g. String "stephen", amount as float, and date in someformat
+    # e.g. String "stephen", amount as float, and date in someformat, date must be in YYYY, MM DD
 
     date = Date.new(date)
 
