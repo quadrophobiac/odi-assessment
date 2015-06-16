74d66219542cc3ae359742aaeb05ff24dcae52cd
stephenfortune <stephen.j.fortune@gmail.com>
2015-06-16 02:25:17 +0100

    lots of comments in the pseudocode for unimplemented getBalanceDate feature

--- transaction_controller.rb
+++ transaction_controller.rb
@@ -17,12 +17,20 @@ class TransactionController
     exchange.save()
   end
 
-  def getBalance(username, year, month, day)
+  def getBalance(user, year, month, day)
     # aim of this is what is the balance on a given date
     chosenDate = Date.new(year, month, day)
-    #establish date span
-    Transaction.between(date: (Transaction.first.date..chosenDate)).count
-    # transaction.first catches the first transaction, not the most chronologically distant, also not inclusive
-    # inclusivity problem I'm encountering is a result of time feature being accurate to the hour of the ranges compared
+    #establish date span and create a 'view' from it for purposes of iterating over
+    user.transactions.between(date: (Transaction.first.date..chosenDate))
+    # transaction.first catches the first transaction, not the most chronologically distant
+    # Edge case fail can arise due to time feature being accurate to the hour of the ranges compared, and all
+    # time entries having a time of 00:00
+
+    # search the above range for the user._id matching within customer_ids array
+    # this may not be necessary with a REPL, but if so consult here http://docs.mongodb.org/manual/tutorial/query-documents/#match-a-specific-element-of-an-array
+
+    # the algorithm needs to have an array where the sequence in the array matches the sequence of dates
+    # ideally return the above queries as a temporary collection and then use Criteria#each method to iterate
+
   end
 end
