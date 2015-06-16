d6fe638353f6fde8abb3743b68fc2757376424a8
stephenfortune <stephen.j.fortune@gmail.com>
2015-06-16 02:23:51 +0100

    included indicative Boolean loops designed to perpetually work with the same set of customers and transactions

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ require './customer'
 require './transaction'
 require './transaction_controller'
 
+Banking = TransactionController.new()
+
 # register loop
 
 # pseudocode
@@ -19,32 +21,38 @@ require './transaction_controller'
 # login loop
 
 # create some accounts
-# if not exists ...
-stephen = Customer.new(name: "stephen", password: "password")
-supervalu = Customer.new(name: "supervalu", password: "password")
-mary = Customer.new(name: "mary", password: "password")
-stephen.save()
-supervalu.save()
-mary.save()
 
-Banking = TransactionController.new()
+if !Customer.exists?
+  stephen = Customer.new(name: "stephen", password: "password")
+  supervalu = Customer.new(name: "supervalu", password: "password")
+  mary = Customer.new(name: "mary", password: "password")
+  stephen.save()
+  supervalu.save()
+  mary.save()
+end
 
+
+if !Transaction.exists?
+  Banking.transfer(stephen, mary, 420.00, Date.new(2015, 04, 9))
+  Banking.transfer(supervalu, stephen, 480.00, Date.new(2015, 04, 30))
+  Banking.transfer(stephen, supervalu, 55.00, Date.new(2015, 04, 30))
+  Banking.transfer(mary, stephen, 45.00, Date.new(2015, 05, 03))
+  Banking.transfer(mary, stephen, 5.00, Date.new(2015, 05, 6))
+  Banking.transfer(stephen, mary, 480.00, Date.new(2015, 05, 9))
+  Banking.transfer(mary, supervalu, 46.00, Date.new(2015, 05, 2))
+  Banking.transfer(stephen, supervalu, 205.00, Date.new(2015, 05, 31))
+  Banking.transfer(supervalu, stephen, 15.00, Date.new(2015, 06, 03))
+  Banking.transfer(stephen, stephen, 25.00, Date.new(2015, 06, 6))
+  Banking.transfer(stephen, stephen, 480.00, Date.new(2015, 06, 8))
+  Banking.transfer(stephen, mary, 420.00, Date.new(2015, 06, 9))
+  Banking.transfer(mary, supervalu, 46.00, Date.new(2015, 06, 2))
+end
 # make some payments
 
-Banking.transfer(supervalu, stephen, 480.00, Date.new(2015, 04, 30))
-Banking.transfer(stephen, supervalu, 55.00, Date.new(2015, 04, 30))
-Banking.transfer(mary, stephen, 45.00, Date.new(2015, 05, 03))
-Banking.transfer(mary, stephen, 5.00, Date.new(2015, 05, 6))
-Banking.transfer(stephen, mary, 480.00, Date.new(2015, 05, 9))
-Banking.transfer(stephen, mary, 420.00, Date.new(2015, 04, 9))
-Banking.transfer(mary, supervalu, 46.00, Date.new(2015, 05, 2))
-Banking.transfer(stephen, supervalu, 205.00, Date.new(2015, 05, 31))
-Banking.transfer(supervalu, stephen, 15.00, Date.new(2015, 06, 03))
-Banking.transfer(stephen, stephen, 25.00, Date.new(2015, 06, 6))
-Banking.transfer(stephen, stephen, 480.00, Date.new(2015, 06, 8))
-Banking.transfer(stephen, mary, 420.00, Date.new(2015, 06, 9))
-Banking.transfer(mary, supervalu, 46.00, Date.new(2015, 06, 2))
 
-puts("Mary's balance is #{mary.getBalance}")
-puts("Stephen's balance is #{stephen.getBalance}")
-puts("Supervalu balance is #{supervalu.getBalance}")
+puts("Mary's current balance is #{mary.getBalance}")
+puts("Stephen's current balance is #{stephen.getBalance}")
+puts("Supervalu current balance is #{supervalu.getBalance}")
+
+Customer.delete_all
+Transaction.delete_all
